dbf0afaba8352bf576e3b4fce84073506a1eb4d5
Jared Muñoz <amunozbaez669@gmail.com>
2025-11-13 22:23:17 -0600

    fix: add fragment on breadcrumbs

--- apps/web/components/app-header.tsx
+++ apps/web/components/app-header.tsx
@@ -1,53 +1,54 @@
 "use client";
 
 import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbList,
-	BreadcrumbLink,
-	BreadcrumbSeparator,
-	BreadcrumbPage,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
 } from "@workspace/ui/components/breadcrumb";
 import { SidebarTrigger } from "@workspace/ui/components/sidebar";
 import { usePathname } from "next/navigation";
 import { ThemeSwitcher } from "./providers";
+import { Fragment } from "react";
 
 export function AppHeader() {
-	const pathname = usePathname();
-	const routes = pathname.split("/").filter((route) => route !== "");
+  const pathname = usePathname();
+  const routes = pathname.split("/").filter((route) => route !== "");
 
-	return (
-		<header className="p-6 pb-0 flex items-center justify-between gap-3">
-			<div className="flex items-center gap-3">
-				<SidebarTrigger />
-				<Breadcrumb>
-					<BreadcrumbList>
-						<BreadcrumbItem>
-							<BreadcrumbLink href="/">
-								Suspense Fallback Debugger
-							</BreadcrumbLink>
-						</BreadcrumbItem>
-						{routes.length > 0 ? (
-							routes.map((route) => (
-								<>
-									<BreadcrumbSeparator key={route} />
-									<BreadcrumbItem key={route}>
-										<BreadcrumbLink href={`/${route}`}>{route}</BreadcrumbLink>
-									</BreadcrumbItem>
-								</>
-							))
-						) : (
-							<>
-								<BreadcrumbSeparator />
-								<BreadcrumbItem>
-									<BreadcrumbPage>Home</BreadcrumbPage>
-								</BreadcrumbItem>
-							</>
-						)}
-					</BreadcrumbList>
-				</Breadcrumb>
-			</div>
-			<ThemeSwitcher />
-		</header>
-	);
+  return (
+    <header className="p-6 pb-0 flex items-center justify-between gap-3">
+      <div className="flex items-center gap-3">
+        <SidebarTrigger />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">
+                Suspense Fallback Debugger
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            {routes.length > 0 ? (
+              routes.map((route) => (
+                <Fragment key={route}>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href={`/${route}`}>{route}</BreadcrumbLink>
+                  </BreadcrumbItem>
+                </Fragment>
+              ))
+            ) : (
+              <>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Home</BreadcrumbPage>
+                </BreadcrumbItem>
+              </>
+            )}
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <ThemeSwitcher />
+    </header>
+  );
 }
